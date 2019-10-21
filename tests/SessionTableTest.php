14b59de56e34b7c6397cf50d2ae9c5b93e6bf889
funsoul <912387963@qq.com>
2019-10-21 10:06:51 +0800

    add phpunit

---
+++ tests/SessionTableTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SocketIO;
+
+use PHPUnit\Framework\TestCase;
+use SocketIO\Storage\Table\SessionTable;
+
+/**
+ * Class SessionTableTest
+ *
+ * @package SocketIO
+ */
+class SessionTableTest extends TestCase
+{
+    /** @var SessionTable */
+    private $sessionTable;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->sessionTable = SessionTable::getInstance();
+    }
+
+    public function testCount()
+    {
+        $this->assertEquals(0, $this->sessionTable->count());
+    }
+
+    public function testPush()
+    {
+        $sid = 'xyz';
+        $fd = 1;
+        $this->assertEquals(true, $this->sessionTable->push($sid, $fd));
+    }
+
+    public function testPop()
+    {
+        $sid = 'xyz';
+        $fd = 1;
+        $this->assertEquals($fd, $this->sessionTable->pop($sid));
+    }
+
+    public function testGet()
+    {
+        $sid = 'xyz';
+        $fd = 1;
+        $this->sessionTable->push($sid, $fd);
+        $this->assertEquals($fd, $this->sessionTable->get($sid));
+        $this->assertEquals($fd, $this->sessionTable->get($sid));
+    }
+}
